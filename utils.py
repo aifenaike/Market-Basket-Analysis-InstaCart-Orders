4061c39b4603da50cfdc22d199bb6e1ab4c1a36e
Ifenaike Alexander <62061374+aifenaike@users.noreply.github.com>
2022-10-04 21:57:16 +0100

    Update Utils script

--- utils.py
+++ utils.py
@@ -13,11 +13,9 @@ import seaborn as sns
 #configure plots from matplotlib
 matplotlib.rcParams['figure.dpi'] = 180 #resolution
 matplotlib.rcParams['figure.figsize'] = (8,6) #figure size
-
-#set seaborn style for plots
-sns.set_style('darkgrid')
-
 color = sns.color_palette()
+
+#Compilation of helper functions
 
 def bar_visual(df, xlabel,ylabel, title):
     fig, ax = plt.subplots(figsize=(15,8))
@@ -42,4 +40,60 @@ def count_viz(df,xlabel,ylabel,title):
 def read(root,path):
     return pd.read_csv(root + path + "/" + path + ".csv")
 
+
+def reduce_memory(df):
     
+    """
+    This function reduce the dataframe memory usage by converting it's type for easier handling.
+    
+    Parameters: Dataframe
+    Return: Dataframe
+    """
+    
+    start_mem_usg = df.memory_usage().sum() / 1024**2 
+    print("Memory usage of properties dataframe is :",start_mem_usg," MB")
+    
+    for col in df.columns:
+        if df[col].dtypes in ["int64", "int32", "int16"]:
+            
+            cmin = df[col].min()
+            cmax = df[col].max()
+            
+            if cmin > np.iinfo(np.int8).min and cmax < np.iinfo(np.int8).max:
+                df[col] = df[col].astype(np.int8)
+            
+            elif cmin > np.iinfo(np.int16).min and cmax < np.iinfo(np.int16).max:
+                df[col] = df[col].astype(np.int16)
+            
+            elif cmin > np.iinfo(np.int32).min and cmax < np.iinfo(np.int32).max:
+                df[col] = df[col].astype(np.int32)
+        
+        if df[col].dtypes in ["float64", "float32"]:
+            
+            cmin = df[col].min()
+            cmax = df[col].max()
+            
+            if cmin > np.finfo(np.float16).min and cmax < np.finfo(np.float16).max:
+                df[col] = df[col].astype(np.float16)
+            
+            elif cmin > np.finfo(np.float32).min and cmax < np.finfo(np.float32).max:
+                df[col] = df[col].astype(np.float32)
+    
+    print("")
+    print("___MEMORY USAGE AFTER COMPLETION:___")
+    mem_usg = df.memory_usage().sum() / 1024**2 
+    print("Memory usage is: ",mem_usg," MB")
+    print("This is ",100*mem_usg/start_mem_usg,"% of the initial size")
+    
+    return df
+    
+
+def overlapped_bar(y_df,x1_df,x2_df, xlabel,ylabel,title):
+    fig, ax = plt.subplots(figsize = (15,8))
+    ax = sns.barplot(y = y_df, x = x1_df, color=color[1], label = "total")
+    ax = sns.barplot(y = y_df, x = x2_df, color=color[3], label = "reordered")
+    ax.set_ylabel("Aisle")
+    ax.set_xlabel("Orders Count")
+    ax.set_title("Total Orders and Reorders From Most Popular Aisles")
+    ax.legend(loc = 4, prop={'size': 12})
+    plt.show()
