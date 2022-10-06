79336b5fe0e56ac819a262ad3f6f758d70bbd726
Ifenaike Alexander <62061374+aifenaike@users.noreply.github.com>
2022-10-06 06:19:45 +0100

    Helper functions for modeling
    Script containing utility functions for modeling

---
+++ modeling_utils.py
@@ -0,0 +1,121 @@
+#Evaluation metric implemented for NN
+def recall_metric(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    possible_positives = K.sum(K.round(K.clip(y_true, 0, 1)))
+    recall = true_positives / (possible_positives + K.epsilon())
+    return recall
+
+def precision_metric(y_true, y_pred):
+    true_positives = K.sum(K.round(K.clip(y_true * y_pred, 0, 1)))
+    predicted_positives = K.sum(K.round(K.clip(y_pred, 0, 1)))
+    precision = true_positives / (predicted_positives + K.epsilon())
+    return precision
+
+def f1_metric(y_true, y_pred):
+    precision = precision_metric(y_true, y_pred)
+    recall = recall_metric(y_true, y_pred)
+    return 2*((precision*recall)/(precision+recall+K.epsilon()))
+
+
+from sklearn.base import BaseEstimator, TransformerMixin
+import numpy as np
+#Target encoder
+class TargetEncoder(BaseEstimator, TransformerMixin):
+    """Target encoder.
+    
+    Replaces categorical column(s) with the mean target value for
+    each category.
+
+    """
+    
+    def __init__(self, cols=None):
+        """Target encoder
+        
+        Parameters
+        ----------
+        cols : list of str
+            Columns to target encode.  Default is to target 
+            encode all categorical columns in the DataFrame.
+        """
+        if isinstance(cols, str):
+            self.cols = [cols]
+        else:
+            self.cols = cols
+        
+        
+    def fit(self, X, y):
+        """Fit target encoder to X and y
+        
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to encode
+        y : pandas Series, shape = [n_samples]
+            Target values.
+            
+        Returns
+        -------
+        self : encoder
+            Returns self.
+        """
+        
+        # Encode all categorical cols by default
+        if self.cols is None:
+            self.cols = [col for col in X 
+                         if str(X[col].dtype)=='object']
+
+        # Check columns are in X
+        for col in self.cols:
+            if col not in X:
+                raise ValueError('Column \''+col+'\' not in X')
+
+        # Encode each element of each column
+        self.maps = dict() #dict to store map for each column
+        for col in self.cols:
+            tmap = dict()
+            uniques = X[col].unique()
+            for unique in uniques:
+                tmap[unique] = y[X[col]==unique].mean()
+            self.maps[col] = tmap
+            
+        return self
+
+        
+    def transform(self, X, y=None):
+        """Perform the target encoding transformation.
+        
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to encode
+            
+        Returns
+        -------
+        pandas DataFrame
+            Input DataFrame with transformed columns
+        """
+        Xo = X.copy()
+        for col, tmap in self.maps.items():
+            vals = np.full(X.shape[0], np.nan)
+            for val, mean_target in tmap.items():
+                vals[X[col]==val] = mean_target
+            Xo[col] = vals
+        return Xo
+            
+            
+    def fit_transform(self, X, y=None):
+        """Fit and transform the data via target encoding.
+        
+        Parameters
+        ----------
+        X : pandas DataFrame, shape [n_samples, n_columns]
+            DataFrame containing columns to encode
+        y : pandas Series, shape = [n_samples]
+            Target values (required!).
+
+        Returns
+        -------
+        pandas DataFrame
+            Input DataFrame with transformed columns
+        """
+        return self.fit(X, y).transform(X, y)
